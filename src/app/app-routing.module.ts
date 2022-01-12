f07401462de87a0a14658713e59a3107e0202ebb
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-01-12 17:27:59 +0600

    Adding main components recipe-form, recipes, recipe-item. Setting up routing between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RecipesComponent},
+  {path: 'add-recipe', component: RecipeFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipeFormComponent,
+    RecipesComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
