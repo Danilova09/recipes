956e41f24eb0b53bfb4873778c0727ebf060a9f9
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-01-12 23:45:01 +0600

    Adding form-validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,10 @@ import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-form.component.css']
 })
 export class RecipeFormComponent implements OnInit {
-
+  recipeForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.recipeForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      recipeDescription: new FormControl('',Validators.required),
+      imgUrl: new  FormControl('', Validators.required),
+      ingredients: new FormControl('', Validators.required),
+      stepImg: new FormControl('', Validators.required),
+      stepDescription: new FormControl('', Validators.required),
+    })
   }
 
+  onSubmit() {
+    console.log(this.recipeForm.controls);
+  }
+
+  fieldHasErrors(fieldName: string, errorType: string) {
+    const field = this.recipeForm.get(fieldName);
+    return Boolean(field && field.touched && field.errors?.[errorType]);
+  }
 }
